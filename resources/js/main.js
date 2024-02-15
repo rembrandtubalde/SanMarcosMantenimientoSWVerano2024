ac210aa61484e386030ebc4058d234185997a2fd
Alexander <frealcale@gmx.es>
2024-02-14 19:11:49 -0500

    bug resolved in resources/js/main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -86,10 +86,21 @@ jQuery(document).ready(function($){
     })      
 });
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  (function(i, s, o, g, r){
+    let a = s.createElement(o);
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function(){
+        let q = i[r].q = i[r].q || []; 
+        q.push(arguments);
+    };
+    i[r].l = 1 * new Date();
+    a.async = 1;
+    a.src = g;
+    let m = s.getElementsByTagName(o)[0];
+    m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+  
 
   ga('create', 'UA-10146041-21', 'auto');
   ga('send', 'pageview');
