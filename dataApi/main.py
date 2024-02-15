c015422aa2520a4cfb9c3f04c57d6b911891cc3c
Bianca <bianca.romero@unmsm.edu.pe>
2024-02-15 17:18:47 -0500

    mant perfectivo api max_duration

--- dataApi/main.py
+++ dataApi/main.py
@@ -1,47 +1,77 @@
 # Importante librerías necesarias
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from typing import Optional
-from typing import List, Tuple
+from typing import List, Tuple, Union
+from enum import Enum,IntEnum
 
 import pandas as pd
+
+class ListaNum(IntEnum):
+    pass
+
+opciones_anio = [year for year in range(1980, 2022)]
+
+class ListaAnios(str, Enum):
+    opcion1 = "2019"
+    opcion2 = "1981"
+    opcion3 = "1982"
+    opcion4 = "1983"
+
+class ListaPlat(str, Enum):
+    opcion1 = "disney"
+    opcion2 = "hulu"
+    opcion3 = "netflix"
+    opcion4 = "amazon"
+
+class ListaTipoDuration(str, Enum):
+    opcion1 = "min"
+    opcion2 = "season"
 
 app = FastAPI()
 
 #http://127.0.0.1:8000
 
-
-
 @app.get("/")
 async def welcome():
-    return "Bienvenido !!! Aquí encontrarás tus películas y series favoritas y mucho más !!!"
+    return "BIENVENIDO A MLOps!!! Usa la ruta /docs para encontrar la documentación de la api MLOps que cuenta con una base de datos de películas y series de entre los años 1980 al 2021 de Netflix, Hulu, Disney+ y Amazon Prime Video, habiendo recuperado poco más de 23mil datos."
 
 @app.get("/index")
 async def index():
-    return "Los cuatro métodos de búsqueda (funciones) son: 1. get_max_duration, 2. get_score_count, 3. get_count_platform y 4. get_actor"
-
-
+    return "Las 4 funcionalidades con las que cuenta esta api: 1. get_max_duration, 2. get_score_count, 3. get_count_platform y 4. get_actor"
 
 df = pd.read_csv('datasets/full_titles.csv')
 
-
 # Creando la función 1: película con mayor duración con filtros opcionales de AÑO, PLATAFORMA Y TIPO DE DURACIÓN
 # (la función debe llamarse get_max_duration(year, platform, duration_type))
+@app.get(
+    "/max_duration",
+    summary = "Película o serie más extensa",
+    description = "Obtén la película con mayor duración o serie con más temporadas aplicando filtros opcionales de año, plataforma y tipo de duración.")
+def get_max_duration(
+    release_year: int = Query(None, description="Ingresa un año entre 1980 y 2021"),
+    platform: str = Query(None, description="Ingresa una plataforma: amazon, disney, hulu o netflix"), 
+    duration_type: ListaTipoDuration = Query(None, description="Selecciona el tipo de duración eligiendo 'min' para pelicula o 'season' para serie")):
 
-@app.get("/max_duration")
-def get_max_duration(year: Optional[int] = None, platform: Optional[str] = None, duration_type: Optional[str] = 'min'):
     # Crear una copia del DataFrame original para evitar modificar los datos originales
     df_copy = df.copy()
     
     try:
 
+        # Validando el año
+        if release_year is not None and (release_year < 1980 or release_year > 2021):
+            raise ValueError("Año fuera de rango. Ingresa un año entre 1980 y 2021.")  
+
         # Validando plataforma correcta
         if platform is not None and platform.lower() not in ['amazon', 'disney', 'hulu', 'netflix']:
             raise ValueError("La plataforma debe ser amazon, disney, hulu o netflix.")
 
-
-        # Aplicando los los filtros opcionales si se especifican
-        if year is not None:
-            df_copy = df_copy[df_copy["release_year"] == year]
+        # Validando plataforma correcta
+        if duration_type is not None and duration_type not in ['min', 'season']:
+            raise ValueError("Comando de duración incorrecto. Ingresa 'min' para película o 'season' para serie.")
+        
+        # Aplicando los filtros opcionales si se especifican
+        if release_year is not None:
+            df_copy = df_copy[df_copy["release_year"] == release_year]
         if platform is not None:
             df_copy = df_copy[df_copy["platform"].str.contains(platform, case=False)]
         if duration_type is not None:
@@ -53,7 +83,7 @@ def get_max_duration(year: Optional[int] = None, platform: Optional[str] = None,
     
         # Creando un diccionario con los datos de la película con mayor duración
         result = {
-            "La película con mayor duración es:" : {
+            "La película/serie con mayor duración es:" : {
             "Título": max_duration_movie["title"],
             "Plataforma": max_duration_movie["platform"],
     #       "Director": max_duration_movie["director"],
@@ -70,9 +100,15 @@ def get_max_duration(year: Optional[int] = None, platform: Optional[str] = None,
 
 # Creando la función 2: cantidad de películas por plataforma con un puntaje mayor a XX en determinado año
 # la función debe llamarse get_score_count(platform, scored, year)
-
-@app.get("/score_count/{platform}/{scored}/{release_year}")
-def get_score_count(platform : str, scored : float, release_year: int):
+@app.get(
+        "/score_count/{platform}/{scored}/{release_year}",
+        summary = "Películas y series según score",
+        description = "Obtén la cantidad de películas con un puntaje mayor al que ingreses en determinado año y plataforma, criterios obligatorios."
+)
+def get_score_count(
+    platform: str,
+    scored: float = Query(None, description="Ingresa un puntaje hasta con dos decimales"),
+    release_year: int = Query(..., description="Ingresa un año entre 1980 y 2021")):
 
     try:        
         # Validando plataforma correcta
@@ -95,9 +131,13 @@ def get_score_count(platform : str, scored : float, release_year: int):
 
 # Creando la función 3: cantidad de películas por plataforma con filtro de PLATAFORMA.
 # La función debe llamarse get_count_platform(platform)
-
-@app.get("/count_platform/{platform}")
-def get_count_platform(platform: str):
+@app.get(
+        "/count_platform/{platform}",
+        summary = "Cantidad de películas y series por plataforma",
+        description = "Obtén la cantidad de películas y series registradas en nuestra base de datos según la plataforma elegida."
+)
+def get_count_platform(
+    platform: str = Query(..., description="Ingresa una plataforma: amazon, disney, hulu o netflix")):
 
     try:        
         # Validando plataforma correcta
@@ -117,9 +157,14 @@ def get_count_platform(platform: str):
     
 
 # Creando la función 4: actor que más se repite según plataforma y año. (La función debe llamarse get_actor(platform, year))
-
-@app.get("/actor")
-def get_actor(platform: str, release_year: int) -> Tuple[int, List[str]]:
+@app.get(
+        "/actor",
+        summary = "Actor con más apariciones",
+        description = "Obtén al actor que aparece en la mayor cantidad de películas y series según plataforma y año elegidos como filtros obligatorios."
+)
+def get_actor(
+    platform: str = Query(..., description="Ingresa una plataforma: amazon, disney, hulu o netflix"),
+    release_year: int = Query(..., description="Ingresa un año entre 1980 y 2021")) -> Tuple[int, List[str]]:
 
     # Validando plataforma correcta
     if platform is not None and platform.lower() not in ['amazon', 'disney', 'hulu', 'netflix']:
@@ -150,10 +195,11 @@ def get_actor(platform: str, release_year: int) -> Tuple[int, List[str]]:
 
 
 # Creando la función 5: Sistema de recomendacion de películas con mayor rating en base al año dado.
-def get_recommendation(year: int) -> str:
+def get_recommendation(
+        release_year: int = Query(..., description="Ingresa un año entre 1980 y 2021")) -> str:
     try:
         # Filtrar las películas por el año especificado
-        movies_of_year = df[df['release_year'] == year]
+        movies_of_year = df[df['release_year'] == release_year]
         
         # Ordenar las películas por rating de forma descendente y seleccionar las 10 mejores
         top_movies = movies_of_year.sort_values(by='score', ascending=False).head(10)
@@ -169,4 +215,3 @@ def get_recommendation(year: int) -> str:
 
     except Exception as e:
         return f"Error: {str(e)}"
-
