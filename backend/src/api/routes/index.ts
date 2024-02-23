91f585fef7b6888231923eac286dad42a16e878d
Raquel Gonza <raquel.stacy28@gmail.com>
2024-02-23 18:46:07 -0500

    agregando las rutas de favoritos a la app

--- backend/src/api/routes/index.ts
+++ backend/src/api/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { register as userRoutesRegister } from './user.routes';
+import { register as favoriteRoutesRegister } from './favorite.routes';
 
 export const registerRoutes = (router: Router) => {
 	userRoutesRegister(router);
+	favoriteRoutesRegister(router);
 };
