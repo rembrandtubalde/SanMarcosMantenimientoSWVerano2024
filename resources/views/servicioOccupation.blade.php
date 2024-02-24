d14e0263fce80e265e92d6e66b992daa4739e824
JeanSuarez12 <jean.suarez@unmsm.edu.pe>
2024-02-23 20:25:28 -0500

    Update servicioOccupation.blade.php

--- resources/views/servicioOccupation.blade.php
+++ resources/views/servicioOccupation.blade.php
@@ -195,10 +195,12 @@
                     <div class="px-4 px-lg-5 my-5">
                         <div class="row gx-4 gx-lg-5 align-items-center">
                             <div class="col-md-6">
-                                <img class="card-img-top mb-5 mb-md-0"
-                                src="{{  $serviceProfile->imagen  }}"
-                                style="width:512px !important;"
-                                alt="..." />
+                               <div class="d-flex justify-content-center">
+                                 <img class="card-img-top mb-5 mb-md-0"
+                                     src="{{  $serviceProfile->imagen  }}"
+                                     style="width:512px !important;"
+                                     alt="..." />
+                                </div>
                             </div>
                             <div class="col-md-6">
                                 <h1 class="display-5 fw-bolder">{{ $serviceProfile->ser_occ_name }}</h1>
