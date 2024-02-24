00885a9cc0e55dc5109c388841adbd3d078d4845
JeanSuarez12 <jean.suarez@unmsm.edu.pe>
2024-02-23 20:26:08 -0500

    Update servicioRetos.blade.php

--- resources/views/servicioRetos.blade.php
+++ resources/views/servicioRetos.blade.php
@@ -132,12 +132,12 @@
         <div class="col-12">
             <section class="py-5">
                 <div class="container px-4 px-lg-5 my-5">
-                    <div class="row gx-4 gx-lg-5 align-items-center">
+                    <div class="row gx-4 gx-lg-5 ">
 
                         <div class="col-md-6">
                             <img class="card-img-top mb-5 mb-md-0"
                                 src="{{  $serviceProfile->imagen  }}"
-                                style="width:512px !important;"
+                                style="width:700px !important; margin-top:60px;"
                                 alt="..." />
 
 
