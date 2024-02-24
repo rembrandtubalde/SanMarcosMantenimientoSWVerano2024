0f75e448a910b60c19cc636e9fcbdc0e5d6d9dc6
Alexander <frealcale@gmx.es>
2024-02-23 21:17:52 -0500

    resolve high issues

--- resources/js/main.js
+++ resources/js/main.js
@@ -70,8 +70,8 @@ jQuery(document).ready(function($){
     
     // jQuery Scroll effect
     $('.navbar-nav li a, .scroll-to-up').bind('click', function(event) {
-        var $anchor = $(this);
-        var headerH = $('.header-area').outerHeight();
+        let $anchor = $(this);
+        let headerH = $('.header-area').outerHeight();
         $('html, body').stop().animate({
             scrollTop : $($anchor.attr('href')).offset().top - headerH + "px"
         }, 1200, 'easeInOutExpo');
--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,5 +1,7 @@
 <?php
 
+define('INVALID_ATTRIBUTE_MESSAGE', 'The selected :attribute is invalid.');
+
 return [
 
     /*
@@ -41,7 +43,7 @@ return [
     'distinct' => 'The :attribute field has a duplicate value.',
     'email' => 'The :attribute must be a valid email address.',
     'ends_with' => 'The :attribute must end with one of the following: :values.',
-    'exists' => 'The selected :attribute is invalid.',
+    'exists' => INVALID_ATTRIBUTE_MESSAGE,
     'file' => 'The :attribute must be a file.',
     'filled' => 'The :attribute field must have a value.',
     'gt' => [
@@ -57,7 +59,7 @@ return [
         'array' => 'The :attribute must have :value items or more.',
     ],
     'image' => 'The :attribute must be an image.',
-    'in' => 'The selected :attribute is invalid.',
+    'in' => INVALID_ATTRIBUTE_MESSAGE,
     'in_array' => 'The :attribute field does not exist in :other.',
     'integer' => 'The :attribute must be an integer.',
     'ip' => 'The :attribute must be a valid IP address.',
@@ -91,7 +93,7 @@ return [
         'array' => 'The :attribute must have at least :min items.',
     ],
     'multiple_of' => 'The :attribute must be a multiple of :value.',
-    'not_in' => 'The selected :attribute is invalid.',
+    'not_in' => INVALID_ATTRIBUTE_MESSAGE,
     'not_regex' => 'The :attribute format is invalid.',
     'numeric' => 'The :attribute must be a number.',
     'password' => 'The password is incorrect.',
